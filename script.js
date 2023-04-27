2f706066627586dd7d887280f646629b519cbfa1
Andrew <a.lucas.webster@gmail.com>
2023-04-27 18:05:57 -0400

    added a button and form for new book

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const myLibrary = [
   {
-    title: 'The Hobbit',
-    Author: 'J. R. R. Tolkein',
+    title: "The Hobbit",
+    Author: "J. R. R. Tolkein",
     pageCount: 304,
-    read: 'yes',
+    read: "yes",
   },
 ];
 
@@ -12,16 +12,29 @@ function Book(title, author, pageCount, read) {
   this.author = author;
   this.pageCount = pageCount;
   this.read = read;
+  this.info = function () {
+    console.log(`${this.title} by ${this.author} is ${this.pageCount} long`);
+  };
 }
 
 function addBookToLibrary() {
   const book = new Book(
-    prompt('enter book tite'),
-    prompt('enter author'),
-    prompt('enter page count'),
-    prompt('enter read status'),
+    prompt("enter book tite"),
+    prompt("enter author"),
+    prompt("enter page count"),
+    prompt("enter read status")
   );
   myLibrary.push(book);
 }
 
+// function displayLibrary () {
+//   myLibrary.forEach()
+// }
 
+// console.log(myLibrary.info);
+
+let newBook = document.querySelector(".new");
+newBook.addEventListener("click", function () {
+  console.log("test");
+  document.getElementById("form").classList.toggle("hide");
+});

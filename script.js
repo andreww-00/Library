b1789569397de616fd7f2eb9818520f548e85e93
Andrew <a.lucas.webster@gmail.com>
2023-05-01 21:29:59 -0400

    general form of the UI

--- script.js
+++ script.js
@@ -2,28 +2,37 @@ const titleElem = document.querySelector("#Title");
 const authorElem = document.querySelector("#Author");
 const pageNumberElem = document.querySelector("#PageCount");
 const readElem = document.querySelector("#read");
+const bookShelf = document.querySelector(".bookshelf");
+const placeHolder = document.querySelector(".placeholder");
+
 
 
 const myLibrary = [
   {
     title: "The Hobbit",
-    Author: "J. R. R. Tolkein",
+    author: "J. R. R. Tolkein",
     pageCount: 304,
-    read: "yes",
+    read: "read",
   },
 ];
+
+placeHolder.textContent = (myLibrary[0].title + ' by ' + myLibrary[0].author + ', ' + myLibrary[0].pageCount + ' pages long, ' + myLibrary[0].read); 
 
 function Book(title, author, pageCount, read) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.read = read;
-  this.info = `${this.title} by ${this.author} is ${this.pageCount} pages long ${this.read}`;
+  this.info = `${this.title} by ${this.author} is ${this.pageCount} pages long, is ${this.read}`;
 }
 
-// function displayLibrary () {
-//   myLibrary.forEach()
-// }
+function displayLibrary () {
+  myLibrary.forEach(Element => {
+    let div = document.createElement('div');
+    div.classList.add("placeholder");
+    bookShelf.appendChild(div);
+  });
+}
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
@@ -39,8 +48,8 @@ form.addEventListener("submit", (e) => {
   }
   let newBook = new Book(titleCont, authorCont, pageNumberCont, readCont);
   myLibrary.push(newBook);
+  displayLibrary();
   document.getElementById("form").classList.toggle("hide");
-  console.log(myLibrary);
 });
 
 let newBook = document.querySelector(".new");
